dc607e475fea483a2c0faa4be5894976bab2b745
malodetz <malo.detz@yandex.ru>
2021-02-02 13:37:06 +0300

    Empty window app

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <QApplication>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
-	QApplication fractals3DApp(argc, argv);
-
-	return fractals3DApp.exec();
+    QApplication fractals3DApp(argc, argv);
+    MainWindow mainWindow;
+    mainWindow.show();
+    return fractals3DApp.exec();
 }
