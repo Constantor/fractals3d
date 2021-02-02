04ca95ca1a3f78c1950b04e93e646903ee57efc8
malodetz <malo.detz@yandex.ru>
2021-02-02 12:56:48 +0300

    Removed modeler and left empty qt app

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <QApplication>
 
-#include "modeler.h"
-
 int main(int argc, char *argv[]) {
 	QApplication fractals3DApp(argc, argv);
-	Modeler modeler;
-	modeler.show();
 
 	return fractals3DApp.exec();
 }
