54c1df9dcaa760c958da9531bc3865128b30d1cd
Stepan Konstantinov <stepan@konstantinov.pl>
2021-02-12 05:46:16 +0300

    Move and in_move added

--- custom_scene.cpp
+++ custom_scene.cpp
@@ -3,17 +3,29 @@
 #include "custom_scene.h"
 
 void CustomScene::pressed(QPoint p) {
+	in_move = true;
 	// doing something with a point p
 	qDebug() << "Pressed: " << p.x() << ' ' << p.y(); // 0, 0 this is bad
 }
 
+void CustomScene::moved(QPoint p) {
+	if(in_move) {
+		qDebug() << "Pressed and moving";
+	}
+}
+
 void CustomScene::released(QPoint p) {
+	in_move = false;
 	// doing something with a point p
 	qDebug() << "Released: " << p.x() << ' ' << p.y(); // 0, 0 this is bad
 }
 
 void CustomScene::mousePressEvent(QGraphicsSceneMouseEvent *event) {
 	emit pressed(event->pos().toPoint());
+}
+
+void CustomScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event) {
+	emit moved(event->pos().toPoint());
 }
 
 void CustomScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *event) {
--- custom_scene.h
+++ custom_scene.h
@@ -7,11 +7,17 @@
 class CustomScene : public QGraphicsScene {
 Q_OBJECT
 protected:
+	bool in_move = false;
+
 	void pressed(QPoint p);
+
+	void moved(QPoint p);
 
 	void released(QPoint p);
 
 	void mousePressEvent(QGraphicsSceneMouseEvent *event);
+
+	void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
 
 	void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 signals:
