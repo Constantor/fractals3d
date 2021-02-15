23736a6cca741a64d0097872fc94f4c41fe82400
malodetz <malo.detz@yandex.ru>
2021-02-15 22:42:02 +0300

    Added borders and precision

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-        QMainWindow(parent), ui(new Ui::MainWindow) {
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+	ui->setupUi(this);
+	//connect(ui->firstCoordBox, SIGNAL(valueChanged(double)), ui->firstCoordBar, SLOT(setValue(int)));
+    //connect(ui->firstCoordBar, SIGNAL(valueChanged(int)), ui->firstCoordBox, SLOT(setValue(double)));
 }
 
 MainWindow::~MainWindow() {
-    delete ui;
+	delete ui;
 }
