6b5868aa236490a5354480454950ab7e5bddbfc3
Anton Sosnin <antsosnin@yandex.ru>
2021-03-05 14:34:17 +0300

    Shaders made from shit and sticks

--- vshader.glsl
+++ vshader.glsl
@@ -1,21 +1,42 @@
+#version 150
+
 #ifdef GL_ES
 // Set default precision to medium
 precision mediump int;
 precision mediump float;
 #endif
 
-uniform mat4 mvp_matrix;
+uniform vec2 verts[6] = vec2[](vec2(-1.0, -1.0), vec2(1.0, -1.0), vec2(1.0, 1.0),
+                                vec2(1.0, 1.0), vec2(-1.0, 1.0), vec2(-1.0, -1.0));
 
-attribute vec4 a_position;
-attribute vec2 a_texcoord;
-
-varying vec2 v_texcoord;
-
-void main() {
-    // Calculate vertex position in screen space
-    gl_Position = mvp_matrix * a_position;
-
-    // Pass texture coordinate to fragment shader
-    // Value will be automatically interpolated to fragments inside polygon faces
-    v_texcoord = a_texcoord;
+void main()
+{
+	gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
 }
+//uniform mat4 mvp_matrix;
+
+//attribute vec4 a_position;
+//attribute vec2 a_texcoord;
+
+//varying vec2 v_texcoord;
+
+////! [0]
+//void main()
+//{
+//	// Calculate vertex position in screen space
+//	gl_Position = mvp_matrix * a_position;
+//}
+////! [0]
+
+
+//#version 150
+
+//uniform mat4 mvp_matrix;
+
+//in vec4 ciPosition;
+
+//void main()
+//{
+//	gl_Position = mvp_matrix * ciPosition;
+//}
+
