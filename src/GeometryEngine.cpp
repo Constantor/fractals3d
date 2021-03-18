f66a56ed1f02f8ce0ef79a8b0fe5372f98dc5ec6
Stepan Konstantinov <stepan@konstantinov.pl>
2021-03-19 02:54:13 +0300

    Commented redutant code

--- src/GeometryEngine.cpp
+++ src/GeometryEngine.cpp
@@ -90,20 +90,20 @@ void GeometryEngine::drawGeometry(QOpenGLShaderProgram *program) {
 	indexBuf.bind();
 
 	// Offset for position
-	quintptr offset = 0;
+	// quintptr offset = 0;
 
 	// Tell OpenGL programmable pipeline how to locate vertex position data
-	int vertexLocation = program->attributeLocation("a_position");
-	program->enableAttributeArray(vertexLocation);
-	program->setAttributeBuffer(vertexLocation, GL_FLOAT, offset, 3, sizeof(VertexData));
+	// int vertexLocation = program->attributeLocation("a_position");
+	//program->enableAttributeArray(vertexLocation);
+	//program->setAttributeBuffer(vertexLocation, GL_FLOAT, offset, 3, sizeof(VertexData));
 
 	// Offset for texture coordinate
-	offset += sizeof(QVector3D);
+	// offset += sizeof(QVector3D);
 
 	// Tell OpenGL programmable pipeline how to locate vertex texture coordinate data
-	int texcoordLocation = program->attributeLocation("a_texcoord");
-	program->enableAttributeArray(texcoordLocation);
-	program->setAttributeBuffer(texcoordLocation, GL_FLOAT, offset, 2, sizeof(VertexData));
+	// int texcoordLocation = program->attributeLocation("a_texcoord");
+	// program->enableAttributeArray(texcoordLocation);
+	// program->setAttributeBuffer(texcoordLocation, GL_FLOAT, offset, 2, sizeof(VertexData));
 
 	// Draw cube geometry using indices from VBO 1
 	glDrawElements(GL_TRIANGLE_STRIP, 34, GL_UNSIGNED_SHORT, nullptr);
